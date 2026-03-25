92fd6142a403e606ebf0b394d747ded18462c1a5
Shubham Yadav <mysquad0007@gmail.com>
2026-03-25 23:09:48 +0530

    compare

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,8 @@ import { authConfig } from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   providers: [
     Credentials({
       async authorize(credentials) {
